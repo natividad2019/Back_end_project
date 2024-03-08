c6a38d557827139304fed7974e32dd962906200f
Kodandarao Chellapilli <briggs2019@go.byuh.edu>
2024-03-08 12:02:27 -0700

    added all the crud operations

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
 const app = express();
 
-app.listen(3000, () => {
-  console.log("server is running on port 3000");
-});
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello from Node API server Updated");
 });
+
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({}); // Corrected variable name from product to products
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Corrected route definition for handling POST requests to "/api/products"
+app.post("/api/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(201).json(product); // Changed status code to 201 for successful creation
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://briggs2019:Kim&Dennis$2019@backenddb.efh0gay.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB",
+    { useNewUrlParser: true, useUnifiedTopology: true } // Added options to MongoClient constructor
+  )
+  .then(() => {
+    console.log("Connected to the database!");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch((error) => { // Added error parameter to catch block
+    console.log("Connection failed!", error); // Included error parameter in console.log
+  });
